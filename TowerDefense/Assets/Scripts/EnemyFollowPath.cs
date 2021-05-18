8f22b9f0f977accbb2dbc755495894950747c3d2
Matheus Guedes <mathheus.gr@gmail.com>
2021-05-18 17:11:06 -0300

    PathSystem

---
+++ TowerDefense/Assets/Scripts/EnemyFollowPath.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyFollowPath : MonoBehaviour
+{
+    public List<GameObject> paths;
+
+    public bool pathCollider = false;
+
+    void Start()
+    {
+        
+    }
+    
+    void Update()
+    {
+        
+    }
+
+
+}
